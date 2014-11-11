56b24dbfa09b71b1bb755bfb38e2592297439fbb
Jeff Casimir <jeff@casimircreative.com>
2014-11-10 21:08:48 -0700

    Adding the base files

---
+++ test/prefix_calculator_test.rb
@@ -0,0 +1,9 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/prefix_calculator'
+
+class PrefixCalculatorTest < Minitest::Test
+  def test_it_exists
+    assert PrefixCalculator
+  end
+end
